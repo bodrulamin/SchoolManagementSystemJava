7b1f33c1af2d178604d23a9033654516a1bec7f1
bodrulamin <bodrulaminiu@gmail.com>
2021-07-31 21:01:34 +0600

    Schools Teacher and Student list added

--- src/com/schoolmanagement/Main.java
+++ src/com/schoolmanagement/Main.java
@@ -4,22 +4,40 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        School school = new School();
         Student maliha = new Student(10, "Maliha", 9);
 
         maliha.setFeesPaid(1000);
         maliha.setFeesPaid(10);
 
+        school.addStudent(maliha);
+
+
         Teacher mehedi = new Teacher(420, "Mehedi", 100);
         mehedi.setSalary(mehedi.getSalary());
         mehedi.setSalary(mehedi.getSalary());
 
-        Teacher yakub = new Teacher(421,"Yakub",200);
+        Teacher yakub = new Teacher(421, "Yakub", 200);
         yakub.setSalary(200);
+
+        school.addTeacher(mehedi);
+        school.addTeacher(yakub);
 
 
         System.out.println("School Earned " + School.totalMoneyEarned);
-        System.out.println("School spent " +School.totalMoneySpent);
+        System.out.println("School spent " + School.totalMoneySpent);
 
         System.out.println("Fund Remaining " + (School.remainingFund()));
+
+        System.out.println("===========Teachers list");
+        for (int i = 0; i < school.teacherList.size(); i++) {
+            System.out.println(school.teacherList.get(i).getName());
+
+        }
+System.out.println("===============Student list");
+        for (int i = 0; i < school.studentList.size(); i++) {
+            System.out.println(school.studentList.get(i).getName());
+
+        }
     }
 }
--- src/com/schoolmanagement/School.java
+++ src/com/schoolmanagement/School.java
@@ -1,10 +1,34 @@
 package com.schoolmanagement;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class School {
     static int totalMoneyEarned;
     static int totalMoneySpent;
+    List<Student> studentList = new ArrayList<>();
+    List<Teacher> teacherList = new ArrayList<>();
 
     public static int remainingFund() {
-      return   totalMoneyEarned - totalMoneySpent;
+        return totalMoneyEarned - totalMoneySpent;
+    }
+
+    public List<Student> getStudentList() {
+        return studentList;
+    }
+
+    public void addStudent(Student student) {
+        this.studentList.add(student);
+    }
+    public void addTeacher(Teacher teacher) {
+        this.teacherList.add(teacher);
+    }
+
+    public List<Teacher> getTeacherList() {
+        return teacherList;
+    }
+
+    public void setTeacherList(List<Teacher> teacherList) {
+        this.teacherList = teacherList;
     }
 }
